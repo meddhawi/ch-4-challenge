dbba57e82059b249bd0c97dc8daa968da12d434c
meddhawi <meddhawi@gmail.com>
2021-10-08 19:08:33 +0700

    added result box and added display mechanism

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
+const rockBack = document.getElementById('rockBack');
+const paperBack = document.getElementById('paperBack');
+const scissorBack = document.getElementById('scissorBack');
+
+//show computer choice
+const rockBackCom = document.getElementById('rockBack-com');
+const paperBackCom = document.getElementById('paperBack-com');
+const scissorBackCom = document.getElementById('scissorBack-com');
+
+const versus = document.getElementById('versus') //to remove VS sign
+const resultBox = document.getElementById('result-box') //to show result sign
+
 //generate choices 
 function compChoice(){
-    //show computer choice
-    const rockBackCom = document.getElementById('rockBack-com');
-    const paperBackCom = document.getElementById('paperBack-com');
-    const scissorBackCom = document.getElementById('scissorBack-com');
-
     const comp = Math.random();
     if (comp < 0.34){
         setTimeout(function(){
             rockBackCom.style.background = "#C4C4C4";
             rockBackCom.style.borderRadius = '30px';
-        }, 200)
+        }, 300)
         return 'rock';
     }else if (comp >= 0.34 && comp <=0.67){
         setTimeout(function(){
             paperBackCom.style.background = "#C4C4C4";
             paperBackCom.style.borderRadius = '30px';
-        },200)
+        }, 300)
         return 'paper';
     } else{
         setTimeout(function(){
             scissorBackCom.style.background = "#C4C4C4";
             scissorBackCom.style.borderRadius = '30px';
-        },200)
+        }, 300)
         
         return 'scissor';
     }
@@ -31,20 +38,46 @@ function compChoice(){
 //setup rules
 function gameResult(player, comp){
     if(player == comp){
-        return 'DRAW'
+        return draw();
     }
     else if(player == 'rock'){
-        return (comp == 'paper') ? 'lose' : 'win';
+        return (comp == 'paper') ? lose() : win();
     }
     else if (player == 'paper'){
-        return (comp == 'scissor') ? 'lose' : 'win';
+        return (comp == 'scissor') ? lose() : win();
     }
     else if(player == 'scissor'){
-        return (comp == 'rock') ? 'lose' : 'win';
+        return (comp == 'rock') ? lose() : win();
     }
 }
 
+//If player lose
+function lose(){
+    console.log("COM WIN!")
+    setTimeout(function(){
+        resultBox.style.display = 'block';
+        versus.style.display = 'none';
+        document.getElementById('result').innerHTML = 'COM WIN!';
+    },600)
+}
 
+function win(){
+    console.log("PLAYER 1 WIN")
+    setTimeout(function(){
+        resultBox.style.display = 'block';
+        versus.style.display = 'none';
+        document.getElementById('result').innerHTML = 'PLAYER 1 WIN!';
+    }, 600)
+}
+
+function draw(){
+    console.log("DRAW")
+    setTimeout(function(){
+        resultBox.style.display = 'block';
+        versus.style.display = 'none';
+        document.getElementById('result').innerHTML = 'DRAW';        
+    },600)
+}
 
 var Enabled = true;
 
@@ -55,17 +88,14 @@ playerRock.addEventListener('click', function(){
     if(Enabled == true){ //no overlap choice
         const playerAction = playerRock.id;
         const compAction = compChoice();
-        const result = gameResult(playerAction, compAction);
+        gameResult(playerAction, compAction);
         
-        const rockBack = document.getElementById('rockBack');
-
         rockBack.style.background = "#C4C4C4";
         rockBack.style.borderRadius = '30px';
 
         console.log('comp :' + compAction);
         console.log("p :" + playerAction);
-        console.log("hasil :" + result);
-
+        
         Enabled = false;
     }
 
@@ -78,15 +108,14 @@ playerPaper.addEventListener('click', function(){
     if(Enabled == true){
         const compAction = compChoice();
         const playerAction = playerPaper.id;
-        const result = gameResult(playerAction, compAction)
-        const paperBack = document.getElementById('paperBack');
+        gameResult(playerAction, compAction);
     
         paperBack.style.background = "#C4C4C4";
         paperBack.style.borderRadius = '30px';
     
         console.log('comp :' + compAction);
         console.log("p :" + playerAction);
-        console.log("hasil :" + result);
+        
     
         Enabled = false;
     }
@@ -98,15 +127,14 @@ playerScissor.addEventListener('click', function(){
     if(Enabled == true){
         const compAction = compChoice();
         const playerAction = playerScissor.id;
-        const result = gameResult(playerAction, compAction)
-        const scissorBack = document.getElementById('scissorBack');
+        gameResult(playerAction, compAction);
     
         scissorBack.style.background = "#C4C4C4";
         scissorBack.style.borderRadius = '30px';
     
         console.log('comp :' + compAction);
         console.log("p :" + playerAction);
-        console.log("hasil :" + result);
+        
     
         Enabled = false;
     }   
